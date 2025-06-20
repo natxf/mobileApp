efeea1410810685a9ff98eae0372fba108c298f7
natxf <157922167+natxf@users.noreply.github.com>
2025-06-20 18:39:52 +0200

    Update supabase.dart

--- lib/utils/supabase.dart
+++ lib/utils/supabase.dart
@@ -1,8 +1,8 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SubabaseCredentials{
-  static const String subabase_uri='https://inwgeosxpjnkcjivhwha.supabase.co';
-  static const String supabase_anon_key='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlud2dlb3N4cGpua2NqaXZod2hhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NjAxMjksImV4cCI6MjA2MzIzNjEyOX0.x5cPcfpyfNWvuqUMCfkjXmf-FCBHiromTtj1QtG4FPs';
+  static const String subabase_uri='URL';
+  static const String supabase_anon_key='ANON_KEY';
 }
 
 Future<void> supaInit() async{
